b8983bca9ce59f6de596b28c5e04d0281a05f6cc
Rich Daley <rich@fishpercolator.co.uk>
2017-11-20 16:29:04 +0000

    All the functionality for portfolio, desktop version

--- components/Project.vue
+++ components/Project.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="portfolio">
     <div>
-      <h2>Portfolio</h2>
+      <h1 class="flush">Portfolio</h1>
       <div id="projects">
         <ul id="projectlist">
-          <li v-for="project in projects"><a @click.prevent="changeProject(project.slug)" :href="`/project/${project.slug}`">{{project.name}}</a></li>
+          <li v-for="project in projects" :class="{current: currentSlug === project.slug}"><a @click.prevent="changeProject(project.slug)" :href="`/project/${project.slug}`">{{project.name}}</a></li>
         </ul>
         <div id="project" v-if="current">
           <img id="projectheader" v-lazy="currentImage" src="$Lazyload.options.loading" :alt="current.name"/>
           <h2>{{current.name}}</h2>
-        </div>
-        <div id="project" v-else>
-          Hello
+          <div class="content" v-html="current.content"></div>
+          <div id="nextprev">
+            <a v-if="prev" @click.prevent="changeProject(prev.slug)" :href="`/project/${prev.slug}`">&laquo; {{prev.name}}</a>
+            <a v-else></a>
+            <a v-if="next" @click.prevent="changeProject(next.slug)" :href="`/project/${next.slug}`">{{next.name}} &raquo;</a>
+          </div>     
         </div>
       </div>
     </div>
@@ -29,7 +32,15 @@ export default {
   },
   data () { return {projects: projects, currentSlug: this.slug} },
   computed: {
-    current () { if (this.currentSlug) return projects.find((p) => this.currentSlug === p.slug) },
+    current () {
+      if (this.currentSlug) {
+        var idx = projects.findIndex((p) => this.currentSlug === p.slug)
+        projects[idx].index = idx
+        return projects[idx]
+      }
+    },
+    next () { return projects[this.current.index + 1] },
+    prev () { return projects[this.current.index - 1] },
     currentImage () { return `/projects/${this.currentSlug}.jpg` }
   },
   methods: {
@@ -37,7 +48,8 @@ export default {
       this.currentSlug = slug
       history.replaceState({}, null, `/project/${slug}`)
     }
-  }
+  },
+  created () { if (!this.currentSlug) this.changeProject(projects[0].slug) }
 }
 </script>
 
@@ -50,22 +62,33 @@ export default {
   }
   #projects {
     display: flex;
-    //flex-wrap: wrap;
     ul#projectlist {
       display: flex;
       flex-direction: column;
       margin: 0;
       padding: 0;
       flex-grow: 1;
+      justify-content: flex-start;
       li {
-        flex-grow: 1;
         list-style-type: none;
         padding: 0;
         text-align: right;
+        display: flex;
+        align-items: center;
+        &:not(:first-child) {
+          border-top: 1px solid $title;
+        }
         a {
           color: $black;
           display: block;
-          padding: 0.5em;
+          padding: 1em 0.5em;
+          flex: 1;
+        }
+        &.current {
+          font-weight: bold;
+          a::before {
+            content: '• ';
+          }
         }
       }
     }
@@ -77,7 +100,13 @@ export default {
         padding: 0.5em;
       }
       img#projectheader {
-        max-width: 100%;
+        width: 100%;
+        height: 271px;
+      }
+      #nextprev {
+        display: flex;
+        padding: 1em;
+        justify-content: space-between;
       }
     }
   }
