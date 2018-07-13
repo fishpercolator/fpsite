433deeb9e0e2d7501e522e48dbe44e1fa1824eac
Rich Daley <rich@fishpercolator.co.uk>
2018-07-13 10:33:49 +0100

    Fix error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,25 +1,20 @@
 <template>
-  <div>
-    <nav-menu/>
-    <logo />
-    <div id="error">
-      <div id="error-inner">
-        <h1>Don't drink that coffee!</h1>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/iSxNP-1VpjE" frameborder="0" allowfullscreen></iframe>
-        <div id="error-desc" v-if="error.statusCode == 404">
-          <h2>This page couldn't be found</h2>
-          <p>Well, this is embarrassing. If you got here by following a link, please <router-link to="/contact">let us know</router-link> so we can fix it.</p>
-        </div>
-        <div v-else>
-          <h2>Error: {{error.message}}</h2>
-          <p>Yerg! Something went wrong.</p>
-        </div>
+  <div id="error">
+    <div id="error-inner">
+      <h1>Don't drink that coffee!</h1>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/iSxNP-1VpjE" frameborder="0" allowfullscreen></iframe>
+      <div id="error-desc" v-if="error.statusCode == 404">
+        <h2>This page couldn't be found</h2>
+        <p>Well, this is embarrassing. If you got here by following a link, please <router-link to="/contact">let us know</router-link> so we can fix it.</p>
       </div>
-      <div class="cta">
-        <router-link to="/">Go home</router-link>
+      <div v-else>
+        <h2>Error: {{error.message}}</h2>
+        <p>Yerg! Something went wrong.</p>
       </div>
     </div>
-    <contact-banner/>
+    <div class="cta">
+      <router-link to="/">Go home</router-link>
+    </div>
   </div>
 </template>
 
